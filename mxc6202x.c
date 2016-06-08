ebe6a44fb275004414394dcd82393dfa4ba5a978
Jelle van der Waa <jelle@vdwaa.nl>
2016-06-08 22:02:55 +0200

    Cleanup driver
    * Remove all unused / unknown code.
    * Add capability to use dev_info.
    * Remove the macro magic for now
    * Test the stub read raw functionality.

--- mxc6202x.c
+++ mxc6202x.c
@@ -20,71 +20,58 @@
 #include <linux/gpio/consumer.h>
 #include <linux/regmap.h>
 #include <linux/iio/sysfs.h>
-#include <linux/iio/trigger.h>
-#include <linux/iio/buffer.h>
-#include <linux/iio/triggered_buffer.h>
-#include <linux/iio/trigger_consumer.h>
 
 #define MXC6202X_DRV_NAME	"mxc6202x"
 #define MXC6202X_REG_CONTROL	0x15
 #define MXC6202X_REG_CTRL	0x00
 #define MXC6202X_REG_DATA	0x01
 
-enum mxc6202x_axis {
-	AXIS_X,
-	AXIS_Y,
-	AXIS_Z,
-};
-
+/* Used for dev_info() */
 struct mxc6202x_data {
-	struct i2c_client *client;
+	struct device *dev;
 };
-
-
-static const unsigned long mxc6202x_scan_masks[] = {
-	BIT(AXIS_X) | BIT(AXIS_Y) | BIT(AXIS_Z),
-	0
-};
-
-#define MXC4005_CHANNEL(_axis, _addr) {				\
-	.type = IIO_ACCEL,					\
-	.modified = 1,						\
-	.channel2 = IIO_MOD_##_axis,				\
-	.address = _addr,					\
-	.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),		\
-	.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_SCALE),	\
-	.scan_index = AXIS_##_axis,				\
-	.scan_type = {						\
-		.sign = 's',					\
-		.realbits = 12,					\
-		.storagebits = 16,				\
-		.shift = 4,					\
-		.endianness = IIO_BE,				\
-	},							\
-}
 
 static const struct iio_chan_spec mxc6202x_channels[] = {
-	MXC4005_CHANNEL(X, 0x1),
-	MXC4005_CHANNEL(Y, 0x1),
-	MXC4005_CHANNEL(Z, 0x1),
+	{
+		.type = IIO_ACCEL,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_SCALE),
+		.modified = 1,
+		.channel2 = IIO_MOD_X,
+	},
+	{
+		.type = IIO_ACCEL,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_SCALE),
+		.modified = 1,
+		.channel2 = IIO_MOD_Y,
+	},
+	{
+		.type = IIO_ACCEL,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_SCALE),
+		.modified = 1,
+		.channel2 = IIO_MOD_Z,
+	}
 };
-
 
 static int mxc6202x_read_raw(struct iio_dev *indio_dev,
 			    struct iio_chan_spec const *chan,
 			    int *val, int *val2, long mask)
 {
-	//dev_info(&ndio_dev->dev.parent, "mxc6202x reading raw");
-	int ret;
-	// How is the mask filled?
+	struct mxc6202x_data *data = iio_priv(indio_dev);
+	dev_info(data->dev, "mxc6202x reading raw");
+
+	switch (mask) {
+		case IIO_CHAN_INFO_RAW:
+			return 1;
+	}
+
+	return -EINVAL;
 }
 
 static const struct iio_info mxc6202x_info = {
-	.driver_module 		= THIS_MODULE,
 	.read_raw 		= mxc6202x_read_raw,
-	//.write_raw 		= // Set's scale for example
-	//.attrs attributes?
-	//
 };
 
 static int mxc6202x_probe(struct i2c_client *client,
@@ -101,9 +88,12 @@ static int mxc6202x_probe(struct i2c_client *client,
 		return -ENOMEM;
 	}
 
+	// No clue why or how this magic works.
+	data = iio_priv(indio_dev);
+	data->dev = &client->dev;
+
 	i2c_set_clientdata(client, indio_dev);
 	indio_dev->dev.parent = &client->dev;
-	indio_dev->available_scan_masks = mxc6202x_scan_masks;
 	indio_dev->name = MXC6202X_DRV_NAME;
 	indio_dev->modes = INDIO_DIRECT_MODE; // ????
 	indio_dev->channels = mxc6202x_channels;
